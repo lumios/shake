39d2b8acdae0f9b23d2d0612e841a620f93cb3f5
Kurisu Brooks <kurisubrooks@gmail.com>
2015-06-26 04:11:30 +1000

    Fixing Package Info

--- src/com/kurisubrooks/eew/java/eew.java
+++ src/com/kurisubrooks/eew/java/eew.java
@@ -1,6 +1,6 @@
 package com.kurisubrooks.eew;
 
-public class main {
+public class eew {
     String api = "http://api.quake.twiple.jp/quake/index.xml";
     String alert = "../resources/alert.mp3";
 }
