a91b6a070551f301cdea72f0b15ee79116f758a8
Kurisu Brooks <kurisubrooks@gmail.com>
2015-06-30 08:03:50 +1000

    Changed XML Strings

--- src/com/kurisubrooks/eew/java/xml/XMLParser.java
+++ src/com/kurisubrooks/eew/java/xml/XMLParser.java
@@ -14,88 +14,101 @@ import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
 public class XMLParser {
-  static final String DATE = "date";
-  static final String ITEM = "item";
-  static final String MODE = "mode";
-  static final String UNIT = "unit";
-  static final String CURRENT = "current";
-  static final String INTERACTIVE = "interactive";
+    // Main Element
+    static final String ITEM = "quake";
 
-  @SuppressWarnings({ "unchecked" })
-  public List<Item> readConfig(InputStream stream) {
-    List<Item> items = new ArrayList<Item>();
-    try {
-      // First, create a new XMLInputFactory
-      XMLInputFactory inputFactory = XMLInputFactory.newInstance();
-      // Setup a new eventReader
-      InputStream in = stream;
-      XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
-      // read the XML document
-      Item item = null;
+    static final String DATE = "eq_date";
+    static final String EPICENTER = "epicenter_code";
+    static final String MAGNITUDE = "magnitude";
+    static final String SEISMIC = "seismic_scale";
+    static final String TEST = "training_type";
+    static final String LATITUDE = "epicenter_lat";
+    static final String LONGITUDE = "epicenter_lng";
+    static final String DEPTH = "depth";
+    static final String ID = "quake_id";
 
-      while (eventReader.hasNext()) {
-        XMLEvent event = eventReader.nextEvent();
+    /*
+    static final String MODE = "mode";
+    static final String UNIT = "unit";
+    static final String CURRENT = "current";
+    static final String INTERACTIVE = "interactive";
+    */
 
-        if (event.isStartElement()) {
-          StartElement startElement = event.asStartElement();
-          // If we have an item element, we create a new item
-          if (startElement.getName().getLocalPart() == (ITEM)) {
-            item = new Item();
-            // We read the attributes from this tag and add the date
-            // attribute to our object
-            Iterator<Attribute> attributes = startElement
-                .getAttributes();
-            while (attributes.hasNext()) {
-              Attribute attribute = attributes.next();
-              if (attribute.getName().toString().equals(DATE)) {
-                item.setDate(attribute.getValue());
-              }
+    @SuppressWarnings({ "unchecked" })
+    public List<Item> readConfig(InputStream stream) {
+        List<Item> items = new ArrayList<Item>();
+        try {
+            // First, create a new XMLInputFactory
+            XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+            // Setup a new eventReader
+            InputStream in = stream;
+            XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
+            // read the XML document
+            Item item = null;
+
+            while (eventReader.hasNext()) {
+                XMLEvent event = eventReader.nextEvent();
+
+                if (event.isStartElement()) {
+                    StartElement startElement = event.asStartElement();
+                    // If we have an item element, we create a new item
+                    if (startElement.getName().getLocalPart() == (ITEM)) {
+                        item = new Item();
+                        // We read the attributes from this tag and add the date
+                        // attribute to our object
+                        Iterator<Attribute> attributes = startElement
+                                .getAttributes();
+                        while (attributes.hasNext()) {
+                            Attribute attribute = attributes.next();
+                            if (attribute.getName().toString().equals(DATE)) {
+                                item.setDate(attribute.getValue());
+                            }
+
+                        }
+                    }
+
+                    if (event.isStartElement()) {
+                        if (event.asStartElement().getName().getLocalPart()
+                                .equals(MODE)) {
+                            event = eventReader.nextEvent();
+                            item.setMode(event.asCharacters().getData());
+                            continue;
+                        }
+                    }
+                    if (event.asStartElement().getName().getLocalPart()
+                            .equals(UNIT)) {
+                        event = eventReader.nextEvent();
+                        item.setUnit(event.asCharacters().getData());
+                        continue;
+                    }
+
+                    if (event.asStartElement().getName().getLocalPart()
+                            .equals(CURRENT)) {
+                        event = eventReader.nextEvent();
+                        item.setCurrent(event.asCharacters().getData());
+                        continue;
+                    }
+
+                    if (event.asStartElement().getName().getLocalPart()
+                            .equals(INTERACTIVE)) {
+                        event = eventReader.nextEvent();
+                        item.setInteractive(event.asCharacters().getData());
+                        continue;
+                    }
+                }
+                // If we reach the end of an item element, we add it to the list
+                if (event.isEndElement()) {
+                    EndElement endElement = event.asEndElement();
+                    if (endElement.getName().getLocalPart() == (ITEM)) {
+                        items.add(item);
+                    }
+                }
 
             }
-          }
-
-          if (event.isStartElement()) {
-            if (event.asStartElement().getName().getLocalPart()
-                .equals(MODE)) {
-              event = eventReader.nextEvent();
-              item.setMode(event.asCharacters().getData());
-              continue;
-            }
-          }
-          if (event.asStartElement().getName().getLocalPart()
-              .equals(UNIT)) {
-            event = eventReader.nextEvent();
-            item.setUnit(event.asCharacters().getData());
-            continue;
-          }
-
-          if (event.asStartElement().getName().getLocalPart()
-              .equals(CURRENT)) {
-            event = eventReader.nextEvent();
-            item.setCurrent(event.asCharacters().getData());
-            continue;
-          }
-
-          if (event.asStartElement().getName().getLocalPart()
-              .equals(INTERACTIVE)) {
-            event = eventReader.nextEvent();
-            item.setInteractive(event.asCharacters().getData());
-            continue;
-          }
+        } catch (XMLStreamException e) {
+            e.printStackTrace();
         }
-        // If we reach the end of an item element, we add it to the list
-        if (event.isEndElement()) {
-          EndElement endElement = event.asEndElement();
-          if (endElement.getName().getLocalPart() == (ITEM)) {
-            items.add(item);
-          }
-        }
-
-      }
-    } catch (XMLStreamException e) {
-      e.printStackTrace();
+        return items;
     }
-    return items;
-  }
 
-} 
+}
