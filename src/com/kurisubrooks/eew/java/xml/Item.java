c3bacdf532f8d2b2d3d8bdd53320f86778d170ef
Kurisu Brooks <kurisubrooks@gmail.com>
2015-06-30 13:52:45 +1000

    Completed XML Receiver

--- src/com/kurisubrooks/eew/java/xml/Item.java
+++ src/com/kurisubrooks/eew/java/xml/Item.java
@@ -1,56 +1,91 @@
 package com.kurisubrooks.eew.java.xml;
 
-
 public class Item {
 
-  private String date;
-  private String epicenter;
-  private String magnitude;
-  private String seismic;
-  private String testMode;
-  private String latitude;
-  private String longitude;
-  private String depth;
-  private String id;
+    private String date;
+    private String epicenter;
+    private String magnitude;
+    private String seismic;
+    private String testMode;
+    private String latitude;
+    private String longitude;
+    private String depth;
+    private String id;
 
-  public String getDate() {return date;}
-  public void setDate(String date) {this.date = date;}
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
 
-  public String getEpicenter() {return epicenter;}
-  public void setEpicenter(String epicenter) {this.epicenter = epicenter;}
+    public String getEpicenter() {
+        return epicenter;
+    }
+    public void setEpicenter(String epicenter) {
+        this.epicenter = epicenter;
+    }
 
-  public String getMagnitude() {return magnitude;}
-  public void setMagnitude(String magnitude) {this.magnitude = magnitude;}
+    public String getMagnitude() {
+        return magnitude;
+    }
+    public void setMagnitude(String magnitude) {
+        this.magnitude = magnitude;
+    }
 
-  public String getSeismic() {return seismic;}
-  public void setSeismic(String seismic) {this.seismic = seismic;}
+    public String getSeismic() {
+        return seismic;
+    }
+    public void setSeismic(String seismic) {
+        this.seismic = seismic;
+    }
 
-  public String getTestMode() {return testMode;}
-  public void setTestMode(String testMode) {this.testMode = testMode;}
+    public String getTestMode() {
+        return testMode;
+    }
+    public void setTestMode(String testMode) {
+        this.testMode = testMode;
+    }
 
-  public String getLatitude() {return latitude;}
-  public void setLatitude(String latitude) {this.latitude = latitude;}
+    public String getLatitude() {
+        return latitude;
+    }
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
 
-  public String getLongitude() {return longitude;}
-  public void setLongitude(String longitude) {this.longitude = longitude;}
+    public String getLongitude() {
+        return longitude;
+    }
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
 
-  public String getDepth() {return depth;}
-  public void setDepth(String depth) {this.depth = depth;}
+    public String getDepth() {
+        return depth;
+    }
+    public void setDepth(String depth) {
+        this.depth = depth;
+    }
 
-  public String getID() {return id;}
-  public void setID(String id) {this.id = id;}
+    public String getID() {
+        return id;
+    }
+    public void setID(String id) {
+        this.id = id;
+    }
 
-  @Override
-  public String toString() {
-    return "Item [" +
-            "date=" + date + ", " +
-            "epicenter=" + epicenter + ", " +
-            "magnitude=" + magnitude + ", " +
-            "seismic=" + seismic + ", " +
-            "testMode=" + testMode + ", " +
-            "latitude=" + latitude + ", " +
-            "longitude=" + longitude + ", " +
-            "depth=" + depth + ", " +
-            "id=" + id + "]";
-  }
+    @Override
+    public String toString() {
+        return "Item [" +
+                "date=" + date + ", " +
+                "epicenter=" + epicenter + ", " +
+                "magnitude=" + magnitude + ", " +
+                "seismic=" + seismic + ", " +
+                "testMode=" + testMode + ", " +
+                "latitude=" + latitude + ", " +
+                "longitude=" + longitude + ", " +
+                "depth=" + depth + ", " +
+                "id=" + id + "]";
+    }
 }
