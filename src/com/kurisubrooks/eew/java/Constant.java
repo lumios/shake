cb50647432d4d6b42b62a08de5d675aa54372fb2
hedgehog1029 <omgredstone@gmail.com>
2015-06-30 21:15:00 +0100

    Whoops

---
+++ src/com/kurisubrooks/eew/java/Constant.java
@@ -0,0 +1,46 @@
+package com.kurisubrooks.eew.java;
+
+import java.util.ArrayList;
+
+import com.kurisubrooks.eew.java.xml.Quake;
+
+public class Constant {
+	private static ArrayList<Quake> rQ = new ArrayList<Quake>();
+	
+	public static boolean compareQuakes(ArrayList<Quake> qs) {
+		
+		if (rQ.isEmpty()) {
+			System.out.println("Recent quake list empty - building quake list");
+			rQ = qs;
+			return false;
+		}
+		
+		if (checkForNewItem(qs, rQ)) {
+			System.out.println("Quake list updated! Triggering event.");
+			try {
+				EEWListener.newQuake(qs.get(0));
+				rQ = qs;
+				return true;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+		return false;
+	}
+	
+	private static boolean checkForNewItem(ArrayList<Quake> aU, ArrayList<Quake> aB) {
+		ArrayList<Quake> aN = aU;
+		aN.removeAll(aB);
+		if (aN.isEmpty()) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public static ArrayList<Quake> getRecentQuakes() {
+		return rQ;
+	}
+}
+ 
