ae5fc117749f6c3f57fe064338bd22c11bde4ec6
hedgehog1029 <omgredstone@gmail.com>
2015-06-30 21:13:35 +0100

    Build internal database then check updates against that database

--- src/com/kurisubrooks/eew/java/EEWListener.java
+++ src/com/kurisubrooks/eew/java/EEWListener.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+
+import notify.Notify;
 
 import com.kurisubrooks.eew.java.xml.*;
 
@@ -24,20 +27,27 @@ public class EEWListener {
     	Runnable listener = new Runnable() {
 			@Override
 			public void run() {
+				System.out.println("BEGIN UPDATE");
 				try {
 					URLConnection eq = ApiURL.openConnection();
 					XMLParser parser = new XMLParser();
 					
-					List<Item> receivedXML = parser.readConfig(eq.getInputStream());
+					List<Quake> receivedXML = parser.readConfig(eq.getInputStream());
 					
-					for (Item item : receivedXML) {
-						System.out.println(item);
-					}
+					Constant.compareQuakes((ArrayList<Quake>) receivedXML);
+					
+					System.out.println("Got quakes data.");
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
     	};
     	scheduler.scheduleWithFixedDelay(listener, 2, 2, TimeUnit.SECONDS);
+    }
+    
+    public static void newQuake(Quake nQ) throws Exception {
+    	Notify n = notify.Notify.getInstance();
+    	//System.out.println("Earthquake Magnitude " + Integer.valueOf(nQ.getMagnitude()) / 10 +  "Epicenter: " + nQ.getEpicenter() + ", Depth: " + nQ.getDepth() + ", Scale: " + nQ.getSeismic());
+    	n.notify(notify.MessageType.INFO, "Earthquake Magnitude " + Integer.valueOf(nQ.getMagnitude()) / 10, "Epicenter: " + nQ.getEpicenter() + ", Depth: " + nQ.getDepth() + ", Scale: " + nQ.getSeismic());
     }
 }
--- src/com/kurisubrooks/eew/java/xml/XMLParser.java
+++ src/com/kurisubrooks/eew/java/xml/XMLParser.java
@@ -2,145 +2,127 @@ package com.kurisubrooks.eew.java.xml;
 
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
 public class XMLParser {
-    static final String ITEM = "quake";
-    static final String DATE = "eq_date";
-    static final String EPICENTER = "epicenter_code";
-    static final String MAGNITUDE = "magnitude";
-    static final String SEISMIC = "seismic_scale";
-    static final String TESTMODE = "training_type";
-    static final String LATITUDE = "epicenter_lat";
-    static final String LONGITUDE = "epicenter_lng";
-    static final String DEPTH = "depth";
-    static final String ID = "quake_id";
+	static final String QUAKE = "quake";
+	static final String DATE = "eq_date";
+	static final String EPICENTER = "epicenter_code";
+	static final String MAGNITUDE = "magnitude";
+	static final String SEISMIC = "seismic_scale";
+	static final String TESTMODE = "training_type";
+	static final String LATITUDE = "epicenter_lat";
+	static final String LONGITUDE = "epicenter_lng";
+	static final String DEPTH = "depth";
+	static final String ID = "quake_id";
 
+	public List<Quake> readConfig(InputStream stream) {
+		List<Quake> items = new ArrayList<Quake>();
+		try {
+			// First, create a new XMLInputFactory
+			XMLInputFactory inputFactory = XMLInputFactory.newInstance();
 
-    @SuppressWarnings({ "unchecked" })
-    public List<Item> readConfig(InputStream stream) {
-        List<Item> items = new ArrayList<Item>();
-        try {
-            // First, create a new XMLInputFactory
-            XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+			// Setup a new eventReader
+			InputStream in = stream;
+			XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
 
-            // Setup a new eventReader
-            InputStream in = stream;
-            XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
+			// read the XML document
+			Quake item = null;
 
-            // read the XML document
-            Item item = null;
+			while (eventReader.hasNext()) {
+				XMLEvent event = eventReader.nextEvent();
 
-            while (eventReader.hasNext()) {
-                XMLEvent event = eventReader.nextEvent();
+				if (event.isStartElement()) {
+					StartElement startElement = event.asStartElement();
 
-                if (event.isStartElement()) {
-                    StartElement startElement = event.asStartElement();
+					// If we have an item element, we create a new item
+					if (startElement.getName().getLocalPart() == (QUAKE)) {
+						item = new Quake();
+					}
 
-                    // If we have an item element, we create a new item
-                    if (startElement.getName().getLocalPart() == (ITEM)) {
-                        item = new Item();
+					if (event.asStartElement().getName().getLocalPart()
+							.equals(DATE)) {
+						event = eventReader.nextEvent();
+						item.setDate(event.asCharacters().getData());
+						continue;
+					}
 
-                        // We read the attributes from this tag and add the date
-                        // attribute to our object
-                        Iterator<Attribute> attributes = startElement
-                                .getAttributes();
-                        while (attributes.hasNext()) {
-                            Attribute attribute = attributes.next();
-                            if (attribute.getName().toString().equals(DATE)) {
-                                item.setDate(attribute.getValue());
-                            }
+					if (event.asStartElement().getName().getLocalPart()
+							.equals(EPICENTER)) {
+						event = eventReader.nextEvent();
+						item.setEpicenter(event.asCharacters().getData());
+						continue;
+					}
 
-                        }
-                    }
+					if (event.asStartElement().getName().getLocalPart()
+							.equals(MAGNITUDE)) {
+						event = eventReader.nextEvent();
+						item.setMagnitude(event.asCharacters().getData());
+						continue;
+					}
 
-                    if (event.isStartElement()) {
-                        if (event.asStartElement().getName().getLocalPart()
-                                .equals(DATE)) {
-                            event = eventReader.nextEvent();
-                            item.setDate(event.asCharacters().getData());
-                            continue;
-                        }
-                    }
+					if (event.asStartElement().getName().getLocalPart()
+							.equals(SEISMIC)) {
+						event = eventReader.nextEvent();
+						item.setSeismic(event.asCharacters().getData());
+						continue;
+					}
 
-                    if (event.asStartElement().getName().getLocalPart()
-                            .equals(EPICENTER)) {
-                        event = eventReader.nextEvent();
-                        item.setEpicenter(event.asCharacters().getData());
-                        continue;
-                    }
+					if (event.asStartElement().getName().getLocalPart()
+							.equals(TESTMODE)) {
+						event = eventReader.nextEvent();
+						item.setTestMode(event.asCharacters().getData());
+						continue;
+					}
 
-                    if (event.asStartElement().getName().getLocalPart()
-                            .equals(MAGNITUDE)) {
-                        event = eventReader.nextEvent();
-                        item.setMagnitude(event.asCharacters().getData());
-                        continue;
-                    }
+					if (event.asStartElement().getName().getLocalPart()
+							.equals(LATITUDE)) {
+						event = eventReader.nextEvent();
+						item.setLatitude(event.asCharacters().getData());
+						continue;
+					}
 
-                    if (event.asStartElement().getName().getLocalPart()
-                            .equals(SEISMIC)) {
-                        event = eventReader.nextEvent();
-                        item.setSeismic(event.asCharacters().getData());
-                        continue;
-                    }
+					if (event.asStartElement().getName().getLocalPart()
+							.equals(LONGITUDE)) {
+						event = eventReader.nextEvent();
+						item.setLongitude(event.asCharacters().getData());
+						continue;
+					}
 
-                    if (event.asStartElement().getName().getLocalPart()
-                            .equals(TESTMODE)) {
-                        event = eventReader.nextEvent();
-                        item.setTestMode(event.asCharacters().getData());
-                        continue;
-                    }
+					if (event.asStartElement().getName().getLocalPart()
+							.equals(DEPTH)) {
+						event = eventReader.nextEvent();
+						item.setDepth(event.asCharacters().getData());
+						continue;
+					}
 
-                    if (event.asStartElement().getName().getLocalPart()
-                            .equals(LATITUDE)) {
-                        event = eventReader.nextEvent();
-                        item.setLatitude(event.asCharacters().getData());
-                        continue;
-                    }
+					if (event.asStartElement().getName().getLocalPart()
+							.equals(ID)) {
+						event = eventReader.nextEvent();
+						item.setID(event.asCharacters().getData());
+						continue;
+					}
+				}
+				// If we reach the end of an item element, we add it to the list
+				if (event.isEndElement()) {
+					EndElement endElement = event.asEndElement();
+					if (endElement.getName().getLocalPart() == (QUAKE)) {
+						items.add(item);
+					}
+				}
 
-                    if (event.asStartElement().getName().getLocalPart()
-                            .equals(LONGITUDE)) {
-                        event = eventReader.nextEvent();
-                        item.setLongitude(event.asCharacters().getData());
-                        continue;
-                    }
-
-                    if (event.asStartElement().getName().getLocalPart()
-                            .equals(DEPTH)) {
-                        event = eventReader.nextEvent();
-                        item.setDepth(event.asCharacters().getData());
-                        continue;
-                    }
-
-                    if (event.asStartElement().getName().getLocalPart()
-                            .equals(ID)) {
-                        event = eventReader.nextEvent();
-                        item.setID(event.asCharacters().getData());
-                        continue;
-                    }
-                }
-                // If we reach the end of an item element, we add it to the list
-                if (event.isEndElement()) {
-                    EndElement endElement = event.asEndElement();
-                    if (endElement.getName().getLocalPart() == (ITEM)) {
-                        items.add(item);
-                    }
-                }
-
-            }
-        } catch (XMLStreamException e) {
-            e.printStackTrace();
-        }
-        return items;
-    }
+			}
+		} catch (XMLStreamException e) {
+			e.printStackTrace();
+		}
+		return items;
+	}
 
 }
