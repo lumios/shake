4e829b7dcb32560957c9f35bc88d2839a3b49eb8
Kurisu <kurisubrooks@gmail.com>
2015-07-12 23:24:48 +1000

    Updating Workbench

--- src/index.js
+++ src/index.js
@@ -1,52 +1,51 @@
-var request = require("request")
-var notifier = require("node-notifier")
-var parse = require("xml2json")
-var path = require("path")
-var moment = require("moment")
-var os = require('os')
-var oldQuakes = []
-require("babel/polyfill")
+var request = require("request");
+var notifier = require("node-notifier");
+var parse = require("xml2json");
+var path = require("path");
+var moment = require("moment");
+var os = require('os');
+var oldQuakes = [];
+require("babel/polyfill");
 
 function getDateTime() {
-    return moment().utcOffset(600).format("DD/MM/YYYY  hh:mm:ss")
+    return moment().utcOffset(600).format("DD/MM/YY h:m:ss");
+    //return moment().utcOffset(600).format("Do MMM YYYY h:m:ssa");
 }
 
-console.log(getDateTime())
+console.log(getDateTime() + ' - Process Started');
 
 function newQuake(quake) {
     if (os.platform() === 'linux' || os.platform() === 'darwin') {
-        notifier.notify( {
+        notifier.notify({
             'title': 'Earthquake Early Warning',
             'subtitle': 'An Earthquake is about to occur in ' + quake.epicenter_code,
             'message': 'Magnitude: ' + quake.magnitude / 10 + ', Seismic Scale: ' + quake.seismic_scale,
             'sound': 'eew',
             'icon': path.join(__dirname, 'icon.png')
-        } )
-    }
-
-    else if (os.platform() == 'win32') {
-        notifier.notify( {
+        });
+    } else if (os.platform() == 'win32') {
+        notifier.notify({
             'title': 'Earthquake Early Warning',
-            'message': 'A Magnitude ' + quake.magnitude / 10 +' Earthquake (Shindo' + quake.seismic_scale + ') is about to occur in ' + quake.epicenter_code + '. Please prepare for strong shaking.',
+            'message': 'A Magnitude ' + quake.magnitude / 10 + ' Earthquake (Shindo' + quake.seismic_scale + ') is about to occur in ' + quake.epicenter_code + '. Please prepare for strong shaking.',
             'icon': path.join(__dirname, 'icon.png')
-        } )
+        });
     }
 
-    console.log(getDateTime() + " [!] Earthquake Detected, Triggering Event")
-    console.log(getDateTime() + " [-] Date/Time: " + moment(quake.eq_date, "X").fromNow() )
-    console.log(getDateTime() + " [-] Magnitude: " + quake.magnitude / 10 + "M")
-    console.log(getDateTime() + " [-] Seismic: " + quake.seismic_scale)
-    console.log(getDateTime() + " [-] Latitude: " + quake.epicenter_lat)
-    console.log(getDateTime() + " [-] Longitude: " + quake.epicenter_lng)
-    console.log(getDateTime() + " [-] Depth: " + quake.depth + "km")
+    console.log(getDateTime() + " [!] Earthquake Detected, Triggering Event");
+    console.log(getDateTime() + " [-] Date/Time: " + moment(quake.eq_date, "X").fromNow());
+    console.log(getDateTime() + " [-] Magnitude: " + quake.magnitude / 10 + "M");
+    console.log(getDateTime() + " [-] Seismic: " + quake.seismic_scale);
+    console.log(getDateTime() + " [-] Latitude: " + quake.epicenter_lat);
+    console.log(getDateTime() + " [-] Longitude: " + quake.epicenter_lng);
+    console.log(getDateTime() + " [-] Depth: " + quake.depth + "km");
 }
 
 function read(error, response, body) {
     if (error) {
-        console.log(error)
+        console.log(error);
     }
 
-    var result = JSON.parse(parse.toJson(body))
+    var result = JSON.parse(parse.toJson(body));
     if (oldQuakes.length !== 0) {
         for (var quake of result.quakes.quake) {
             var notfound = true
@@ -62,7 +61,7 @@ function read(error, response, body) {
             }
         }
     }
-    oldQuakes = result.quakes.quake
+    oldQuakes = result.quakes.quake;
 }
 
 /*
@@ -72,7 +71,7 @@ function search() {
 */
 
 function search() {
-    request('http://api.quake.twiple.jp/quake/index.xml', read)
+    request('http://api.quake.twiple.jp/quake/index.xml', read);
 }
 
-setInterval(search, 2000)
+setInterval(search, 2000);
