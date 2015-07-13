e2310c11fc984ed83bb08f82dc33eb3d35d8478c
Penagwin <langpenguin@gmail.com>
2015-07-13 19:34:13 -0400

    Preparing for the build scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,20 @@
 var uglify = require('gulp-uglify');
 var gulp = require("gulp");
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var builder = require('gulp-node-webkit-builder');
+
 gulp.task('compress', function() {
-  return gulp.src('src/*.js')
+
+  return browserify('./src/client.js').ignore("nw.gui").bundle().pipe(source('bundle.js')).pipe(buffer())
     .pipe(uglify())
-    .pipe(gulp.dest('src/bin/*.js'));
+    .pipe(gulp.dest('./src/bin/'));
+});
+gulp.task('scripts', function() {
+  return gulp.src(['./src/**/*'])
+    .pipe(builder({
+        version: 'v0.9.2',
+        platforms: ['win64']
+     }));
 });
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-var request = require("request");
-var notifier = require("node-notifier");
-var parse = require("xml2json");
-var path = require("path");
-var moment = require("moment");
-var os = require('os');
-var oldQuakes = [];
-require("babel/polyfill");
+var request = require("request")
+var notifier = require("node-notifier")
+var parse = require("xml2json")
+var path = require("path")
+var moment = require("moment")
+var os = require('os')
+var oldQuakes = []
+var $ = require('zepto-browserify').$
+require("babel/polyfill")
 
 function getDateTime() {
-    return moment().utcOffset(600).format("DD/MM/YY h:m:ss");
-    //return moment().utcOffset(600).format("Do MMM YYYY h:m:ssa");
+    return moment().utcOffset(600).format("DD/MM/YY h:m:ss")
+    //return moment().utcOffset(600).format("Do MMM YYYY h:m:ssa")
 }
 
-console.log(getDateTime() + ' - Process Started');
+console.log(getDateTime() + ' - Process Started')
 
 function newQuake(quake) {
     if (os.platform() === 'linux' || os.platform() === 'darwin') {
@@ -22,30 +23,30 @@ function newQuake(quake) {
             'message': 'Magnitude: ' + quake.magnitude / 10 + ', Seismic Scale: ' + quake.seismic_scale,
             'sound': 'eew',
             'icon': path.join(__dirname, 'icon.png')
-        });
+        })
     } else if (os.platform() == 'win32') {
         notifier.notify({
             'title': 'Earthquake Early Warning',
             'message': 'A Magnitude ' + quake.magnitude / 10 + ' Earthquake (Shindo' + quake.seismic_scale + ') is about to occur in ' + quake.epicenter_code + '. Please prepare for strong shaking.',
             'icon': path.join(__dirname, 'icon.png')
-        });
+        })
     }
 
-    console.log(getDateTime() + " [!] Earthquake Detected, Triggering Event");
-    console.log(getDateTime() + " [-] Date/Time: " + moment(quake.eq_date, "X").fromNow());
-    console.log(getDateTime() + " [-] Magnitude: " + quake.magnitude / 10 + "M");
-    console.log(getDateTime() + " [-] Seismic: " + quake.seismic_scale);
-    console.log(getDateTime() + " [-] Latitude: " + quake.epicenter_lat);
-    console.log(getDateTime() + " [-] Longitude: " + quake.epicenter_lng);
-    console.log(getDateTime() + " [-] Depth: " + quake.depth + "km");
+    console.log(getDateTime() + " [!] Earthquake Detected, Triggering Event")
+    console.log(getDateTime() + " [-] Date/Time: " + moment(quake.eq_date, "X").fromNow())
+    console.log(getDateTime() + " [-] Magnitude: " + quake.magnitude / 10 + "M")
+    console.log(getDateTime() + " [-] Seismic: " + quake.seismic_scale)
+    console.log(getDateTime() + " [-] Latitude: " + quake.epicenter_lat)
+    console.log(getDateTime() + " [-] Longitude: " + quake.epicenter_lng)
+    console.log(getDateTime() + " [-] Depth: " + quake.depth + "km")
 }
 
 function read(error, response, body) {
     if (error) {
-        console.log(error);
+        console.log(error)
     }
 
-    var result = JSON.parse(parse.toJson(body));
+    var result = JSON.parse(parse.toJson(body))
     if (oldQuakes.length !== 0) {
         for (var quake of result.quakes.quake) {
             var notfound = true
@@ -61,7 +62,7 @@ function read(error, response, body) {
             }
         }
     }
-    oldQuakes = result.quakes.quake;
+    oldQuakes = result.quakes.quake
 }
 
 /*
@@ -71,7 +72,7 @@ function search() {
 */
 
 function search() {
-    request('http://api.quake.twiple.jp/quake/index.xml', read);
+    request('http://api.quake.twiple.jp/quake/index.xml', read)
 }
 
-setInterval(search, 2000);
+setInterval(search, 2000)
