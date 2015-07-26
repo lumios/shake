db3f61900732562e4feb0d9ae68401e1868eb514
Penagwin <langpenguin@gmail.com>
2015-07-26 17:13:02 -0600

    cli

--- src/index.js
+++ src/index.js
@@ -1,52 +1,56 @@
-var request = require("request")
-var notifier = require("node-notifier")
-var parse = require('xml2js').parseString
-var path = require("path")
-var moment = require("moment")
-var os = require('os')
-var oldQuakes = []
-var $ = require('zepto-browserify').$
-require("babel/polyfill")
+
+
+
+var request = require("request");
+var notifier = require("node-notifier");
+var parseString = require('xml2js').parseString;
+var path = require("path");
+var moment = require("moment");
+var os = require('os');
+var oldQuakes = [];
+var ipc = require('ipc')
+
+require("babel/polyfill");
 
 function getDateTime() {
-    return moment().utcOffset(600).format("DD/MM/YY h:m:ss")
-    //return moment().utcOffset(600).format("Do MMM YYYY h:m:ssa")
+    return moment().utcOffset(600).format("DD/MM/YY h:m:ss");
+    //return moment().utcOffset(600).format("Do MMM YYYY h:m:ssa");
 }
 
-console.log(getDateTime() + ' - Process Started')
+console.log(getDateTime() + ' - Process Started');
 
 function newQuake(quake) {
-    if (os.platform() === 'linux' || os.platform() === 'darwin') {
+    if (false && os.platform() === 'linux' || os.platform() === 'darwin') {
         notifier.notify({
             'title': 'Earthquake Early Warning',
             'subtitle': 'An Earthquake is about to occur in ' + quake.epicenter_code,
             'message': 'Magnitude: ' + quake.magnitude / 10 + ', Seismic Scale: ' + quake.seismic_scale,
             'sound': 'eew',
-            'icon': path.join(__dirname, 'resources/icon.png')
-        })
-    } else if (os.platform() == 'win32') {
+            'icon': path.join(__dirname, 'icon.png')
+        });
+    } else if (false && os.platform() == 'win32') {
         notifier.notify({
             'title': 'Earthquake Early Warning',
             'message': 'A Magnitude ' + quake.magnitude / 10 + ' Earthquake (Shindo' + quake.seismic_scale + ') is about to occur in ' + quake.epicenter_code + '. Please prepare for strong shaking.',
-            'icon': path.join(__dirname, 'resources/icon.png')
-        })
+            'icon': path.join(__dirname, 'icon.png')
+        });
     }
-
-    console.log(getDateTime() + " [!] Earthquake Detected, Triggering Event")
-    console.log(getDateTime() + " [-] Date/Time: " + moment(quake.eq_date, "X").fromNow())
-    console.log(getDateTime() + " [-] Magnitude: " + quake.magnitude / 10 + "M")
-    console.log(getDateTime() + " [-] Seismic: " + quake.seismic_scale)
-    console.log(getDateTime() + " [-] Latitude: " + quake.epicenter_lat)
-    console.log(getDateTime() + " [-] Longitude: " + quake.epicenter_lng)
-    console.log(getDateTime() + " [-] Depth: " + quake.depth + "km")
+    ipc.send('alert' );
+    console.log(getDateTime() + " [!] Earthquake Detected, Triggering Event");
+    console.log(getDateTime() + " [-] Date/Time: " + moment(quake.eq_date, "X").fromNow());
+    console.log(getDateTime() + " [-] Magnitude: " + quake.magnitude / 10 + "M");
+    console.log(getDateTime() + " [-] Seismic: " + quake.seismic_scale);
+    console.log(getDateTime() + " [-] Latitude: " + quake.epicenter_lat);
+    console.log(getDateTime() + " [-] Longitude: " + quake.epicenter_lng);
+    console.log(getDateTime() + " [-] Depth: " + quake.depth + "km");
 }
 
 function read(error, response, body) {
     if (error) {
-        console.log(error)
+        console.log(error);
     }
-
-    parse(parse.toJson(body), function(err, result){
+ 	ipc.send('alert')
+    parseString(xml, function(err, result) {
         if (oldQuakes.length !== 0) {
             for (var quake of result.quakes.quake) {
                 var notfound = true
@@ -62,7 +66,7 @@ function read(error, response, body) {
                 }
             }
         }
-        oldQuakes = result.quakes.quake
+        oldQuakes = result.quakes.quake;
     });
 }
 
@@ -73,7 +77,20 @@ function search() {
 */
 
 function search() {
-    request('http://api.quake.twiple.jp/quake/index.xml', read)
+    request('http://api.quake.twiple.jp/quake/index.xml', read);
 }
 
-setInterval(search, 2000)
+setInterval(function() {
+    console.log("run")
+    newQuake({
+        "eq_date": "1437066950",
+        "epicenter_code": "476",
+        "magnitude": "47",
+        "seismic_scale": "03",
+        "training_type": "0",
+        "epicenter_lat": "33.3",
+        "epicenter_lng": "139.5",
+        "depth": "10",
+        "quake_id": "20150717021556"
+    })
+}, 15000);
