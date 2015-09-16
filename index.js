2130160b932d44520862cc7fc08068be238ea6dc
justin97530 <jcraftr1@gmail.com>
2015-09-16 09:45:29 +0530

    Moved parser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var socket = require('socket.io-client')('http://ssh.kurisubrooks.com:3080');
 var BrowserWindow = require('browser-window'); // Electron Browser Windows
+var parser = require('./parser.js');           // Code to parse EEW data
 var colors = require('colors');                // Terminal Text Formatting
 var fse = require('fs-extra');                 // File System Extras
 var osenv = require('osenv');                  // OS Specific Globals
@@ -15,7 +16,7 @@ else var notifier = require('node-notifier');
 
 var date = new Date();
 var lang = 'en';
-var local = JSON.parse(fs.readFileSync('./resources/lang.json') + '');
+var locale = JSON.parse(fs.readFileSync('./resources/lang.json') + '');
 var copy = './resources/audio/';
 var paste = osenv.home() + '/Library/Sounds/';
 var alertWindow = null;
@@ -34,8 +35,8 @@ socket.on('connect', function() {
     console.log(('[*] Connected to Server').success);
 
 	notifier.notify({
-		'title': local.en.title,
-		'message': local.en.connect,
+		'title': locale.en.title,
+		'message': locale.en.connect,
 		'sound': false
 	});
 });
@@ -49,8 +50,8 @@ socket.on('disconnect', function() {
     console.log(('[!] Socket Dropped').error);
 
 	notifier.notify({
-		'title': local.en.title,
-		'message': local.en.disconnect,
+		'title': locale.en.title,
+		'message': locale.en.disconnect,
 		'sound': false
 	});
 });
@@ -59,32 +60,21 @@ function parse(input) {
     var data = JSON.parse(input);
 
     try {
-        //            0    1    2    3    4     5     6     7     8
-        var scale = ['1', '2', '3', '4', '5-', '5+', '6-', '6+', '7'];
-        if (data.revision == 1)							var sound_string = 'nhk-alert';
-        else if (data.type == 2 || data.situation == 1)	var sound_string = 'simple';
-        else if (scale.indexOf(data.seismic_en) >= 4)	var sound_string = 'keitai';
-        else 											var sound_string = 'nhk';
-
-        if (data.situation == 1)    var situation_string = local.en.units.final;
-        else                        var situation_string = '#' + data.revision;
-
-        if (data.type == 1 || data.situation == 2) {
-            var subtitle_template 	= '';
-            var message_template 	= local.en.cancelled;
-        } else {
-            var subtitle_template 	= data.epicenter_en;
-            var message_template 	= local.en.units.magnitude + ': ' + data.magnitude + ', ' + local.en.units.seismic + ': ' + data.seismic_en;}
+        var sound_string = parser.soundString(data);
+        var situation_string = parser.situationString(data, locale);
+        var template = parser.template(data, locale);
+        var subtitle_template = template[0];
+        var message_template = template[1];
 
         console.log(('[~] ' + data.earthquake_time + ' - ' + data.epicenter_en).yellow);
-        console.log(('[~] ' + local.en.units.update + ' ' + situation_string + ', ' + local.en.units.magnitude + ': ' + data.magnitude + ', ' + local.en.units.seismic + ': ' + data.seismic_en).yellow);
+        console.log(('[~] ' + locale.en.units.update + ' ' + situation_string + ', ' + locale.en.units.magnitude + ': ' + data.magnitude + ', ' + locale.en.units.seismic + ': ' + data.seismic_en).yellow);
 
 		// Night Mode Check
 		if (date.getHours() >= '06' || scale.indexOf(data.seismic_en) >= 5) {
 			// Mac Day Notification
 			if (process.platform === 'darwin') {
 	            notifier.notify({
-	                'title': local.en.title,
+	                'title': locale.en.title,
 	                'subtitle': subtitle_template,
 	                'message': message_template,
 	                'sound': sound_string,
@@ -94,7 +84,7 @@ function parse(input) {
 			// Linux & Windows Day Notification
 	        } else {
 	            notifier.notify({
-	                'title': local.en.title,
+	                'title': locale.en.title,
 	                'subtitle': subtitle_template,
 	                'message': message_template,
 					'icon': path.join(__dirname, './resources/icon.png'),
@@ -107,7 +97,7 @@ function parse(input) {
 			// Mac Night Notifiction
 			if (process.platform === 'darwin') {
 	            notifier.notify({
-	                'title': local.en.title,
+	                'title': locale.en.title,
 	                'subtitle': subtitle_template,
 	                'message': message_template,
 	                'sound': false,
@@ -117,7 +107,7 @@ function parse(input) {
 			// Linux & Windows Night Notification
 	        } else {
 	            notifier.notify({
-	                'title': local.en.title,
+	                'title': locale.en.title,
 	                'subtitle': subtitle_template,
 	                'message': message_template,
 					'icon': path.join(__dirname, './resources/icon.png'),
@@ -156,7 +146,7 @@ function parse(input) {
 		}
 
     } catch (err) {
-        notifier.notify({'title': local.en.title, 'message': local.en.error + ': ' + err.message, 'sound': false});
+        notifier.notify({'title': locale.en.title, 'message': locale.en.error + ': ' + err.message, 'sound': false});
     }
 }
 
---
+++ parser.js
@@ -0,0 +1,31 @@
+exports.soundString = function(data) {
+    //            0    1    2    3    4     5     6     7     8
+    var scale = ['1', '2', '3', '4', '5-', '5+', '6-', '6+', '7'];
+    
+    if (data.revision == 1) {
+        return 'nhk-alert';
+    } else if (data.type == 2 || data.situation == 1) {
+        return 'simple';
+    } else if (scale.indexOf(data.seismic_en) >= 4)	{
+        return 'keitai';
+    } else {
+        return 'nhk';
+    }
+}
+
+exports.situationString = function(data, locale) {
+    if (data.situation == 1) {
+        var situation_string = locale.en.units.final;
+    } else {
+        var situation_string = '#' + data.revision;
+    }
+}
+
+exports.template = function(data, locale) {
+    if (data.type == 1 || data.situation == 2) {
+        return ['', locale.en.cancelled];
+    } else {
+        var message_template = locale.en.units.magnitude + ': ' + data.magnitude + ', ' + locale.en.units.seismic + ': ' + data.seismic_en;
+        return [data.epicenter_en, message_template];
+    }
+}
