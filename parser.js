e2201d3abee214dddd52b9b822c360d6fcb7a5aa
kurisubrooks <kurisubrooks@gmail.com>
2015-09-20 04:39:04 +1000

    Removed Old Sounds

--- parser.js
+++ parser.js
@@ -2,8 +2,8 @@ exports.soundString = function(data) {
     //            0    1    2    3    4     5     6     7     8
     //var scale = ['1', '2', '3', '4', '5-', '5+', '6-', '6+', '7'];
 
-    if (data.revision == 1) return 'nhk-alert';
-    else if (data.type == 1 || data.situation == 2) return 'simple';
+    //if (data.revision == 1) return 'nhk-alert';
+    if (data.type == 1 || data.situation == 2) return 'simple';
     // else if (scale.indexOf(data.seismic_en) >= 4) return 'keitai';
     else return 'nhk';
 };
