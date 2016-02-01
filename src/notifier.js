911534e931fc41bd43aaaddea365576828f494bf
Kurisu Brooks <kurisubrooks@gmail.com>
2016-02-02 01:53:21 +1100

    Update node-notifier

--- src/notifier.js
+++ src/notifier.js
@@ -1,36 +1,37 @@
 const path = require('path');
-const logger = require('lumios-toolkit');
-var notifier;
+const crimson = require('crimson');
+const macifier = require('node-notifier').NotificationCenter;
+const notifier = require('node-notifier');
 
-if (process.platform === 'darwin') {
-	notifier = require(path.join(__dirname, '../lib', 'node-notifier'));
-} else notifier = require('node-notifier');
+var notifier_mac = new macifier({
+    customPath: path.join(__dirname, '../', 'lib', 'shake.app', 'Contents', 'MacOS', 'terminal-notifier')
+});
 
-exports.notify = function(title, subtitle, message, sound, callback) {
-	if (process.platform === 'darwin') {
-		return notifier.notify({
-			'title': title,
-			'subtitle': subtitle,
-			'message': message,
-			'sound': sound,
-			'time': 8000,
-			'wait': true
-		}, function (error, response) {
-			if (error) logger.error('Error: ' + error);
-			callback;
-		});
-	} else {
-		return notifier.notify({
-			'title': title,
-			'subtitle': subtitle,
-			'message': message,
-			'sound': sound,
-			'urgency': 'critical',
-			'time': 8000,
-			'icon': path.join(__dirname, 'resources', 'IconLogo.png')
-		}, function (error, response) {
-			if (error) logger.error('Error: ' + error);
-			callback;
-		});
-	}
+exports.notify = (title, subtitle, message, sound, callback) => {
+    if (process.platform === 'darwin') {
+        return notifier_mac.notify({
+            'title': title,
+            'subtitle': subtitle,
+            'message': message,
+            'sound': sound,
+            'time': 8000,
+            'wait': true
+        }, (error, response) => {
+            if (error) crimson.error('Error: ' + error);
+            if (callback) callback();
+        });
+    } else {
+        return notifier.notify({
+            'title': title,
+            'subtitle': subtitle,
+            'message': message,
+            'sound': sound,
+            'urgency': 'critical',
+            'time': 8000,
+            'icon': path.join(__dirname, 'resources', 'IconLogo.png')
+        }, (error, response) => {
+            if (error) crimson.error('Error: ' + error);
+            if (callback) callback();
+        });
+    }
 };
